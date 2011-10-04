337f4d7cc2b50bc85eaed1e54da2b5b1a72ead2a
Matt Bornski <matt@bornski.com>
2011-10-03 21:14:58 -0700

    variable pip location

--- launchd.py
+++ launchd.py
@@ -19,11 +19,14 @@ def bootstrap(packages={}):
     
     # The default package manager which comes with most Python installations, easy_install, is not capable of many
     # things I regularly depend on, like one-line installs from source repos.  Get pip instead.
+    pip_location = None
     if len(packages) > 0:
         try:
             subprocess.check_call(shlex.split('pip --version'))
+            pip_location = 'pip'
         except (OSError, subprocess.CalledProcessError):
             subprocess.check_call(shlex.split('easy_install --user --script-dir . pip'))
+            pip_location = './pip'
 
     # Install packages you requested.  We probably don't have sudo privileges so we'll do this into user-writable
     # locations.  I do not advise installing packages your program will depend on _at run time_ into user locations,
@@ -35,7 +38,7 @@ def bootstrap(packages={}):
         try:
             __import__(package_name)
         except ImportError:
-            subprocess.check_call(shlex.split('./pip install --user' + package_location))
+            subprocess.check_call(shlex.split(pip_location + ' install --user' + package_location))
             package_refresh_required = True
     if package_refresh_required:
         reload()
