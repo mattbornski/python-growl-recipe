6e8e6bd214ff10f8b8abdee972c784ceba1a0f32
Matt Bornski <matt@bornski.com>
2011-10-03 20:03:26 -0700

    More generic launchd functions please

--- GrowlDemo.py
+++ GrowlDemo.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python
 
+import os.path
+
+SETTINGS = {
+    'namespace':'com.bornski',
+    'vendor':'Bornski',
+    'program':'GrowlDemo',
+    'icon':os.path.join(os.path.dirname(__file__), 'icon.png'),
+    # What non-default packages need to be installed?
+    'packages':{
+        'Growl':'py-Growl',
+    },
+    # Which function in this file should be called?
+    'entry_point':'run',
+}
+
 def run():
-    print 'foo'
-    print 'bar'
+    import Growl
+    
+    icon = open(SETTINGS['icon'], 'rb').read()
+    notifier = Growl.GrowlNotifier(applicationName=SETTINGS['program'], notifications=['alive'], applicationIcon=icon)
+    notifier.register()
+    notifier.notify('alive', SETTINGS['program'], '<-- Now that\'s going to be a big growl')
+
+import launchd
+launchd.handle(**SETTINGS)
--- launchd.py
+++ launchd.py
@@ -10,18 +10,6 @@ import sys
 
 VENDOR = 'Bornski'
 PROGRAM = 'Growl Demo'
-CONFIG = {
-    'run_folder':os.path.abspath(os.path.dirname(__file__)),
-    'application_folder':os.path.abspath(os.path.expanduser('~/Library/Application Support/{vendor}'.format(vendor=VENDOR))),
-    'launchd_folder':os.path.abspath('/System/Library/LaunchAgents'),
-    'namespace':'com.{vendor}'.format(vendor=VENDOR.lower()),
-    'application_name':'{program}'.format(program=''.join(PROGRAM.split())),
-}
-# Absolute filenames
-CONFIG['application_icon'] = os.path.join(CONFIG['run_folder'], 'heart_tattoo.png')
-# Relative filenames
-CONFIG['plist_filename'] = os.path.join(CONFIG['launchd_folder'], '.'.join([CONFIG['namespace'], CONFIG['application_name'], 'plist']))
-CONFIG['bash_filename'] = CONFIG['application_name']
 
 def bootstrap(packages={}):
     # Bootstrap an environment sufficient to install our application.
@@ -46,36 +34,36 @@ def bootstrap(packages={}):
             subprocess.check_call(shlex.split('pip install {package_location} --user'.format(package_location=package_location)))
             package_refresh_required = True
     if package_refresh_required:
-        os.execv(os.path.abspath(__file__), sys.argv)
+        os.execv(os.path.abspath(sys.argv[0]), sys.argv)
 
-def installed():
-    # Check that the application folder exists and that the launchd configuration is valid.
-    return (subprocess.call(shlex.split('launchctl list {namespace}.{application_name}'.format(**CONFIG))) == 0)
+def installed(settings={}):
+    # Check that the application folder exists and that the launchd settingsuration is valid.
+    return (subprocess.call(shlex.split('launchctl list {namespace}.{program}'.format(**settings))) == 0)
 
-def uninstall():
+def uninstall(settings={}):
     bootstrap({'shmac':'git+http://github.com/mattbornski/shmac.git#egg=shmac'})
     import shmac
     
-    # Remove config from launchd and remove the application folder.
+    # Remove settings from launchd and remove the application folder.
     try:
-        subprocess.call(shlex.split('launchctl unload {plist_filename}'.format(**CONFIG)))
+        subprocess.call(shlex.split('launchctl unload {plist_filename}'.format(**settings)))
     except OSError:
         pass
-    shmac.sudo('rm {plist_filename}'.format(**CONFIG))
+    shmac.sudo('rm {plist_filename}'.format(**settings))#, icon=settings['icon'], name=' '.join([settings['vendor'], settings['program']]))
     try:
-        shutil.rmtree(CONFIG['application_folder'])
+        shutil.rmtree(settings['application_folder'])
     except OSError:
         pass
 
-def install(packages={}):
+def install(packages={}, settings={}):
     bootstrap({'shmac':'git+http://github.com/mattbornski/shmac.git#egg=shmac'})
     import shmac
     
     # Create the environment necessary to run our application
     cwd = os.getcwd()
     try:
-        shutil.copytree(CONFIG['run_folder'], CONFIG['application_folder'], ignore=lambda src, names: [n for n in names if n in ['.git', '.gitignore']])
-        os.chdir(CONFIG['application_folder'])
+        shutil.copytree(settings['run_folder'], settings['application_folder'], ignore=lambda src, names: [n for n in names if n in ['.git', '.gitignore']])
+        os.chdir(settings['application_folder'])
         if len(packages) > 0:
             try:
                 import virtualenv
@@ -83,55 +71,75 @@ def install(packages={}):
                 subprocess.check_call(shlex.split('pip install virtualenv --user'))
             subprocess.check_call(shlex.split('virtualenv --no-site-packages env'))
             subprocess.check_call(shlex.split('pip install ' + ' '.join(packages.values()) + ' -E env'))
-        with open(CONFIG['bash_filename'], 'w') as bash:
+            
+        # Make a bash script for launchd to invoke.
+        bash_filename = settings['program'] + '.sh'
+        with open(bash_filename, 'w') as bash:
             bash.write('''#!/bin/bash
 {virtualenv}
-echo "import {application_name} ; {application_name}.run()" | /usr/bin/env python -
-'''.format(virtualenv='source env/bin/activate\n' if len(packages) > 0 else '', **CONFIG))
-        os.chmod(CONFIG['bash_filename'], 0755)
+echo "import {program} ; {program}.run()" | /usr/bin/env python -
+'''.format(virtualenv='source env/bin/activate\n' if len(packages) > 0 else '', **settings))
+        os.chmod(bash_filename, 0755)
         
         # Create the launchd file for our application
-        temp_plist_filename = os.path.basename(CONFIG['plist_filename'])
+        temp_plist_filename = os.path.basename(settings['plist_filename'])
         with open(temp_plist_filename, 'w') as f:
             f.write('''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
   <dict>
     <key>Label</key>
-    <string>{namespace}.{application_name}</string>
+    <string>{namespace}.{program}</string>
     <key>Program</key>
-    <string>{application_folder}/{application_name}</string>
+    <string>{application_folder}/{bash_filename}</string>
     <key>WorkingDirectory</key>
     <string>{application_folder}</string>
     <key>ProgramArguments</key>
     <array>
-      <string>{application_name}</string>
+      <string>{bash_filename}</string>
     </array>
     <key>RunAtLoad</key>
     <true/>
     <key>StandardOutPath</key>
-    <string>{application_folder}/{application_name}.log</string>
+    <string>{application_folder}/{program}.log</string>
     <key>StandardErrorPath</key>
-    <string>{application_folder}/{application_name}.log</string>
+    <string>{application_folder}/{program}.log</string>
   </dict>
-</plist>'''.format(**CONFIG))
-        shmac.sudo('cp {temp_plist_filename} {plist_filename}'.format(temp_plist_filename=temp_plist_filename, **CONFIG))
+</plist>'''.format(bash_filename=bash_filename, **settings))
+        shmac.sudo('cp {temp_plist_filename} {plist_filename}'.format(temp_plist_filename=temp_plist_filename, **settings))#, icon=settings['icon'], name=' '.join([settings['vendor'], settings['program']]))
         os.remove(temp_plist_filename)
     finally:
         os.chdir(cwd)
 
-def run():
+def run(settings):
     # Ask launchd to start the program.
-    subprocess.check_call(shlex.split('launchctl load {plist_filename}'.format(**CONFIG)))
-    subprocess.check_call(shlex.split('launchctl start {namespace}.{application_name}'.format(**CONFIG)))
+    subprocess.check_call(shlex.split('launchctl load {plist_filename}'.format(**settings)))
+    subprocess.check_call(shlex.split('launchctl start {namespace}.{program}'.format(**settings)))
 
-if __name__ == '__main__':
+def restart(settings):
+    subprocess.check_call(shlex.split('launchctl stop {namespace}.{program}'.format(**settings)))
+    subprocess.check_call(shlex.split('launchctl start {namespace}.{program}'.format(**settings)))
+
+def handle(**settings):
+    assert('namespace' in settings)
+    assert('vendor' in settings)
+    assert('program' in settings)
+    settings['run_folder'] = os.path.abspath(os.path.dirname(__file__))
+    settings['application_folder'] = os.path.abspath(os.path.expanduser('~/Library/Application Support/{vendor}'.format(**settings)))
+    settings['launchd_folder'] = os.path.abspath('/System/Library/LaunchAgents')
+    settings['plist_filename'] = os.path.join(settings['launchd_folder'], '.'.join([settings['namespace'], settings['program'], 'plist']))
+
     commands = (sys.argv[1:] if len(sys.argv) > 1 else ['install', 'run'])
     for command in commands:
         if command == 'uninstall':
-            uninstall()
+            uninstall(settings=settings)
         elif command == 'install':
-            if not installed():
-                install(packages={})
+            if not installed(settings=settings):
+                install(settings=settings, packages={
+                    # importable name : installable name
+                    'Growl':'py-Growl',
+                })
         elif command == 'run':
-            run()
+            run(settings=settings)
+        elif command == 'restart':
+            restart(settings=settings)
